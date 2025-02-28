f44d7eeeccdf53c836d547db8755e33ab86d5383
Matthias Huber <matthi.hub@gmail.com>
2025-02-28 16:10:03 +0100

    misc

--- src/test/java/example/com/idauptime/IdauptimeApplicationTests.java
+++ src/test/java/example/com/idauptime/IdauptimeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IdauptimeApplicationTests {
+class IdaUptimeApplicationTests {
 
 	@Test
 	void contextLoads() {
